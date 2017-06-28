9eb274af375fe939e2446b45fd8e19119e60838d
mguldner <mathieuguldner.emn@gmail.com>
2017-06-28 15:04:51 +0200

    Create Leaf2.java

---
+++ src/machinelearning/decisiontree/data/Leaf2.java
@@ -0,0 +1,18 @@
+package machinelearning.decisiontree.data;
+
+public class Leaf2<T> implements Tree2<T>{
+
+  private T value;
+  
+  public Leaf2(T value) {
+    this.value = value;
+  }
+
+  public T getValue() {
+    return value;
+  }
+
+  public void setValue(T value) {
+    this.value = value;
+  }
+}
