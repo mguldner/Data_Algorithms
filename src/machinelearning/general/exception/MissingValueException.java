f5c0e3403d7ad5580923e9ae3ce619778e2e7b5a
mguldner <mathieuguldner.emn@gmail.com>
2017-06-29 09:17:53 +0200

    Update MissingValueException.java

--- src/machinelearning/general/exception/MissingValueException.java
+++ src/machinelearning/general/exception/MissingValueException.java
@@ -4,8 +4,8 @@ package machinelearning.general.exception;
 public class MissingValueException extends Exception {
 
   private static final long serialVersionUID = 1L;
-  
+
   public MissingValueException(String message){
-      super(message);
-}
+    super(message);
+  }
 }
