b959ab5d9263bd8a37fab6c8288b164663685c6c
mguldner <mathieuguldner.emn@gmail.com>
2017-07-24 08:34:45 +0200

    Update DoubleRandomizer.java

--- src/machinelearning/kmeans/DoubleRandomizer.java
+++ src/machinelearning/kmeans/DoubleRandomizer.java
@@ -15,6 +15,15 @@ public class DoubleRandomizer implements Randomizer<Double> {
         rangeMax = data.stream().reduce((i,j) -> i > j ? i : j).get();
         r = new Random();
     }
+    
+    @Override
+    public List<Double> getNSemiRandomObject(int n) {
+        List<Double> randomDoubles = new ArrayList<>();
+        for(int i=0; i<n; i++){
+            randomDoubles.add(getRandomObject());
+        }
+        return randomDoubles;
+    }
 
     @Override
     public List<Double> getNRandomObject(int n) {
