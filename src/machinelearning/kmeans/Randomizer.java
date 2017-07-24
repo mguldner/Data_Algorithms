91396dcea1ca28a25cf0097631ae532874fef57b
mguldner <mathieuguldner.emn@gmail.com>
2017-07-24 08:44:35 +0200

    Update Randomizer.java

--- src/machinelearning/kmeans/Randomizer.java
+++ src/machinelearning/kmeans/Randomizer.java
@@ -3,8 +3,6 @@ package machinelearning.kmeans;
 import java.util.List;
 
 public interface Randomizer<T> {
-
-    List<T> getNSemiRandomObject(int n);
     
     List<T> getNRandomObject(int n);
     
