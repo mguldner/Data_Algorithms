b07c5e2d397b32e9b2b67c33b7d5b65f1cf30acf
mguldner <mathieuguldner.emn@gmail.com>
2017-07-26 14:12:40 +0200

    Refactoring of kmeans algorithm

--- src/machinelearning/kmeans2/KMeansAlgorithm.java
+++ src/machinelearning/kmeans2/KMeansAlgorithm.java
@@ -20,7 +20,12 @@ public class KMeansAlgorithm<T> {
         this.data = data;
         this.genericTool = genericTool;
     }
-
+    
+    public void run(){
+        init();
+        compute();
+    }
+    
     public void init(){
         int dataSize = this.data.size();
         int clustersInitSize = dataSize / 3;
@@ -35,71 +40,38 @@ public class KMeansAlgorithm<T> {
     }
 
     public void compute(){
-        List<T> previousCentroids = this.getCentroids();
         boolean hasChanged = true;
         while(hasChanged){
             hasChanged = false;
+            List<T> previousCentroids = this.getCentroids();
             this.clusters.stream().forEach(Cluster<T>::clearPoints);
+            for(int i=0; i<this.data.size(); i++){
+                setToClosestCluster(this.data.get(i));
+            }
+            this.clusters.stream().forEach(Cluster<T>::assignCentroid);
+            List<T> newCentroids = this.getCentroids();
+            for(T centroid : previousCentroids){
+                if(!this.genericTool.areTheSame(previousCentroids.get(i), newCentroids.get(i)){
+                    hasChanged = true;
+            }
         }
-    }
-
-    public void run(){
-        init();
-        compute();
     }
 
     public List<T> getCentroids(){
         return this.clusters.stream().map(Cluster<T>::getCentroid).collect(Collectors.toList());
     }
-
-    public Map<Integer, ArrayList<T>> apply(List<T> data, int k){
-        Map<Integer, T> centroids = new HashMap<Integer, T>();
-        Map<Integer, ArrayList<T>> associatedPoints = new HashMap<Integer, ArrayList<T>>();
-        List<T> randomizedData = randomizer.getNRandomObject(k);
-        for(int i=0; i<k; i++){
-            centroids.put(i, randomizedData.get(i));
-        }
-        boolean hasChanged = true;
-        while(hasChanged){
-            hasChanged = false;
-            for(int i=0; i<k; i++){
-                associatedPoints.put(i, new ArrayList<T>());
-            }
-            for(int i=0; i<data.size(); i++){
-                int closestCluster = getClosestCluster(centroids, data.get(i));
-                associatedPoints.get(closestCluster).add(data.get(i));
-            }
-            for(int i=0; i<k; i++){
-                if(associatedPoints.get(i).isEmpty()){
-                    centroids.put(i, randomizer.getRandomObject());
-                }
-                else{
-                    hasChanged = meanTool.
-                            T mean = this.meanTool.getMean(associatedPoints.get(i));
-                    if(!mean.isEquivalentTo(centroids.get(i))){
-                        hasChanged = true;
-                        centroids.put(i, mean);
-                    }
-                }
-            }
-        }
-        for(Map.Entry<Integer, T> entry : centroids.entrySet()){
-            System.out.println(entry.getKey() + " : " + entry.getValue() + "\n");
-        }
-        return associatedPoints;
-    }
-
-    public int getClosestCluster(Map<Integer, T> centroids, T dataPoint){
+    
+    public void setToClosestCluster(T dataPoint){
         double minDistance = Double.MAX_VALUE;
         int closest = -1;
-        for(int i=0; i<centroids.size(); i++){
-            double distance = this.comparator.distanceBetween(centroids.get(i), dataPoint);
+        for(int i=0; i<this.clusters.size(); i++){
+            double distance = this.genericToool.distanceBetween(this.clusters.get(i).getCentroid(), dataPoint);
             if(distance < minDistance){
                 minDistance = distance;
                 closest = i;
             }
         }
-        return closest;
+        this.clusters.get(i).addPoint(dataPoint);
     }
 
 }
