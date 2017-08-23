dbcd9556e1284ed4a5c977669b6ad2fa35762fcc
mguldner <mathieuguldner.emn@gmail.com>
2017-08-23 16:42:33 +0200

    Create ClustersScoringTest.java

---
+++ src/machinelearning/kmeans2/ClustersScoringTest.java
@@ -0,0 +1,18 @@
+package machinelearning.kmeans2;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class ClustersScoringTest {
+
+    @Test
+    public void testBasicScore() {
+        
+    }
+    
+    @Test
+    public void testSilhouetteScore() {
+        
+    }
+}
